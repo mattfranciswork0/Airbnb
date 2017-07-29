42dda5bc78f1932b215dbec31a45ec716a957356
matthew <mattfrancis888@gmail.com>
2017-07-29 09:26:04 +0700

    Able to send registration data to mysql database
    ...
    Add: POJOs to send and retrieve data
    Add: DatabaseInterface to create methods that will call routes.
    Add: UserRegistrationRequest class to act as a JSON object.
    Add: Retrofit calls in WelcomeActivity.
    Add: Test Button in activity_welcome to send data.
     ...

---
+++ app/src/main/java/com/example/toshiba/airbnb/POJOUserRegistration.java
@@ -0,0 +1,78 @@
+package com.example.toshiba.airbnb;
+
+
+/**
+ * reated by Owner on 2017-06-19.
+ */
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class POJOUserRegistration {
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("first_name")
+    @Expose
+    private String firstName;
+    @SerializedName("last_name")
+    @Expose
+    private String lastName;
+    @SerializedName("email")
+    @Expose
+    private String email;
+    @SerializedName("password")
+    @Expose
+    private String password;
+    @SerializedName("age")
+    @Expose
+    private Integer age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
--- app/src/main/java/com/example/toshiba/airbnb/WelcomeActivity.java
+++ app/src/main/java/com/example/toshiba/airbnb/WelcomeActivity.java
@@ -1,13 +1,24 @@
 package com.example.toshiba.airbnb;
 
+
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -34,7 +45,43 @@ public class WelcomeActivity extends AppCompatActivity {
             }
         });
 
+        final DatabaseInterface retrofit = new Retrofit.Builder()
+                .baseUrl("http://192.168.2.89:3000/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build().create(DatabaseInterface.class);
+        Button bPost = (Button) findViewById(R.id.bPost);
+        bPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UserRegistrationRequest userRegistrationRequest = new UserRegistrationRequest(
+                        "Matt", "Louis", "test@gmail.com", "lp334564a", 84);
 
+                Call<POJOUserRegistration> call = retrofit.insertUserRegistration(userRegistrationRequest);
+                call.enqueue(new Callback<POJOUserRegistration>() {
+                    @Override
+                    public void onResponse(Call<POJOUserRegistration> call, Response<POJOUserRegistration> response) {
+                        Log.d("blue", "Data is sent");
+                        if (response.isSuccessful()) {
+                        } else {
+                            try {
+                                JSONObject jObjError = new JSONObject(response.errorBody().string());
+                                jObjError.getString("message");
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<POJOUserRegistration> call, Throwable t) {
+                        Log.d("blue", "fail");
+                    }
+                });
+
+            }
+        });
 
     }
 }
